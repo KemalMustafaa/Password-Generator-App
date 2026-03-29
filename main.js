df6af3faca157330a83d4f22e619b01ed60d3345
KemalMustafaa <mustafakemal1505@gmail.com>
2026-03-29 22:03:30 +0700

    menambahkan function createPassword() pada main

--- main.js
+++ main.js
@@ -7,3 +7,16 @@ const number = "0987654321";
 const symbol = "!@#$%^&*())_+<>?/[]{}";
 
 const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += number[Math.floor(Math.random() * number.length)];
+  password += symbol[Math.floor(Math.random() * symbol.length)];
+
+  while (length > password.length) {
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+  }
+  passwordBox.value = password;
+}
