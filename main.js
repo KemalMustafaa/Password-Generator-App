9cc53282041973f138b1f01439e0c4c1949c71f4
KemalMustafaa <mustafakemal1505@gmail.com>
2026-03-29 21:58:06 +0700

    menambahkan file main.js dan menambahkan variable didalamnya

---
+++ main.js
@@ -0,0 +1,9 @@
+const passwordBox = document.getElementById("password");
+const length = 12;
+
+const upperCase = "QWERTYUIOPASDFGHJKLZXCVBNM";
+const lowerCase = "qwertyuiopasdfghjklzxcvbnm";
+const number = "0987654321";
+const symbol = "!@#$%^&*())_+<>?/[]{}";
+
+const allChars = upperCase + lowerCase + number + symbol;
